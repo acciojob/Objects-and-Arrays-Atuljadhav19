69557141ed981dd076f16078f77dc6e4157b1635
Atuljadhav19 <133120719+Atuljadhav19@users.noreply.github.com>
2023-06-13 01:16:09 -0700

    created a submission

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const person = {
   age: 80,
 };
 
+let team = players;
+
+team1 = players.copy();
+
+cap1 = person.copy();
+
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
 window.players = players;
