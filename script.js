1e07610bea0698e2b0de136f092efd939c917a2b
Atuljadhav19 <133120719+Atuljadhav19@users.noreply.github.com>
2023-06-13 01:22:42 -0700

    created a submission

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ let cap1 = Object.assign({}, person);
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 
-// window.players = players;
-// window.person = person;
-// window.team = team;
-// window.team1 = team1;
-// window.cap1 = cap1;
+window.players = players;
+window.person = person;
+window.team = team;
+window.team1 = team1;
+window.cap1 = cap1;
